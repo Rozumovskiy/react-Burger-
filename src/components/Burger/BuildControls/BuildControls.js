d17f4c6bc11f5934dcbb696b79d953bbcc88fab0
Bogdan <bogdanrozumovskij@gmail.com>
2019-07-20 14:31:55 +0300

    Adding the Order Button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import classes from './BuildControls.css';
-import BuildControl from './BuildControl/BuildControl';
+import BuildControl from './/BuildControl/BuildControl';
 
 const controls = [
     { label: 'Salad', type: 'salad'},
@@ -12,11 +12,18 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        {controls.map(() => (
-            ctrl => (
-                <BuildControl key={ctrl.label} label={ctrl.label} />
-            )
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map(ctrl => (
+                <BuildControl 
+                    key={ctrl.label} 
+                    label={ctrl.label}
+                    added={ () => props.ingredientAdded(ctrl.type)} 
+                    removed={ () => props.ingredientRemoved(ctrl.type)}
+                    disabled={props.disabled[ctrl.type]} />
         ))}
+        <button 
+            className={classes.OrderButton}
+            disabled={!props.purchasable}>ORDER NOW</button>
     </div>
 );
 
