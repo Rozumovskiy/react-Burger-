91b2d83cb38a42f79cf204a2a612bd4a6c1b336e
Bogdan <bogdanrozumovskij@gmail.com>
2019-07-24 21:45:13 +0300

    Improving Performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button.js'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingrediens)
+class OrderSummary extends Component {
+
+    // Should be functional
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingrediens)
         .map(igKey => {
             return (
             <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingrediens[igKey]}
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingrediens[igKey]}
             </li>);
         });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: </strong>{props.price.toFixed(2)}</p>
-            <p>Continue to Checkout</p>
-            <Button btnType="Danger" clicked={props.purchaseCancel}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Aux>
-    )
-};
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: </strong>{this.props.price.toFixed(2)}</p>
+                <p>Continue to Checkout</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancel}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Aux>
+        );
 
-export default orderSummary;
+    }
+}
+
+export default OrderSummary;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../Aux/Aux';
 import styles from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar.js';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer.js';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar.js';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer.js';
 
 class Layout extends Component {
 
